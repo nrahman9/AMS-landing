d14fe68aaae92267e24b59ede48d3961ad23f503
nrahman9 <naymur97@gmail.com>
2021-09-10 08:55:15 +0600

    third commit

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.css'],
 })
 export class CounterComponent implements OnInit {
+  counterUp = 0;
+  counter = 0;
+  counterUpStop: any = setInterval(() => {
+    this.counterUp++;
+    if (this.counterUp == 97) {
+      clearInterval(this.counterUpStop);
+    }
+  }, 50);
+  counterStop: any = setInterval(() => {
+    this.counter++;
+    if (this.counter == 30) {
+      clearInterval(this.counterStop);
+    }
+  }, 50);
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
