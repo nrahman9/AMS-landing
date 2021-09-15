e7cabd6d3f041a2e63ace1471dff94e1025825a1
nrahman9 <naymur97@gmail.com>
2021-09-15 21:43:50 +0600

    routing implement in footers modules.

--- src/app/footers/footers-routing.module.ts
+++ src/app/footers/footers-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { TeamsComponent } from './teams/teams.component';
+const routes: Routes = [
+  {
+    path: 'TeamsComponent',
+    component: TeamsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class FootersRoutingModule { }
+export class FootersRoutingModule {}
