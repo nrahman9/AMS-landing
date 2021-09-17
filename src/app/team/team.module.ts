23842a3548e1208bc6c6524f75462af4f6b9baf6
nrahman9 <naymur97@gmail.com>
2021-09-17 23:11:36 +0600

    tittle component make reuseable.

--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { TeamsComponent } from './teams/teams.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [TeamsComponent],
-  imports: [CommonModule, NgbModule],
+  imports: [CommonModule, NgbModule, SharedModule],
   exports: [TeamsComponent],
 })
 export class TeamModule {}
