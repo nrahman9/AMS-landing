0749d51bf1cd2b3c2e9013729db06f5a9678a8d1
nrahman9 <naymur97@gmail.com>
2021-11-16 18:34:28 +0600

    add some description.

--- src/app/team/teams/teams.component.ts
+++ src/app/team/teams/teams.component.ts
@@ -9,12 +9,15 @@ export class TeamsComponent implements OnInit {
     {
       title: 'Sefat Anam',
       description: 'Team Leader',
-
+      description2:
+        'Making API with .NET5, Connect API with Frontend & Deploying.',
       img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
     },
     {
       title: 'Naymur Rahman',
       description: 'Team member',
+      description2:
+        'Making Frontend layout & component design using angular material & bootstrap.',
       img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
     },
   ];
